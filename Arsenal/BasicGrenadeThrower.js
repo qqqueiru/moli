068da0f61921c8fe587fa95bcf7da3c1fc1c195b
Miguel Queiruga <miquepon@gmail.com>
2023-02-19 13:56:02 +0100

    Organizing weapon classes in Arsenal folder.

---
+++ Arsenal/BasicGrenadeThrower.js
@@ -0,0 +1,34 @@
+class BasicGrenadeThrower {
+    #grenadesLeft = 10000;  // TODO dejar en 10
+    #character;
+    #rate = 100;
+    #lastThrowingTime = 0;
+    constructor(character) {
+        this.#character = character;
+    }
+    throw() {
+        if (!this.checkIfCanThrowSinceLastThrowing()) { return null; }
+        if (this.#grenadesLeft <= 0) { return null; }
+        this.#grenadesLeft--;
+        return new Grenade(
+            10,  // damage
+            20,  // speed
+            this.#character.getFaceDirection(),  // direction
+            this.#character.getPos().addConst(new Point(0, this.#character.getShootingHeight())),  // startingPosition
+            1920  // maxDistance
+        );
+    }
+
+    addGrenades(amount) {
+        this.#grenadesLeft += amount;
+    }
+
+    checkIfCanThrowSinceLastThrowing() {
+        let canThrow = false;
+        if (Date.now() - this.#lastThrowingTime > this.#rate) {
+            canThrow = true;
+            this.#lastThrowingTime = Date.now();
+        }
+        return canThrow;
+    }
+}
---
+++ Arsenal/Weapon.js
@@ -0,0 +1,24 @@
+class Weapon {
+    name = "NONE";  // pistol, machine-gun, shotgun
+    ammoLeft = -1;  // -1: infinite
+    ammoWhenFound = 20;
+    character;
+    rate = 10;  // Tasa máxima de disparo en ms (no se podrá disparar a mayor frecuencia)
+    #lastShootingTime = 0;
+    constructor(character) {
+        this.character = character;
+    }
+
+    shoot() {
+        alert("Método shoot de la clase base Weapon...");
+    }
+
+    checkIfCanShootSinceLastShooting() {
+        let canShoot = false;
+        if (Date.now() - this.#lastShootingTime > this.rate) {
+            canShoot = true;
+            this.#lastShootingTime = Date.now();
+        }
+        return canShoot;
+    }
+}
